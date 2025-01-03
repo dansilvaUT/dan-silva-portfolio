18227b8c9c194b9ee72d99f218c2068ffa5716f9
dansilvaUT <dan13silv@gmail.com>
2025-01-03 12:34:43 -0700

    use hash router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter as Router, Routes, Route } from "react-router";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/dan-silva-portfolio" element={<Home />} />
         <Route path="/dan-silva-portfolio/about" element={<About />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
