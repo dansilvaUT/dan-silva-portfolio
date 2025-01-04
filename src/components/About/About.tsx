8439b631f256b7a98b81cc7e81acac6c7732e735
dansilvaUT <dan13silv@gmail.com>
2025-01-03 21:40:47 -0700

    update about/projects.

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,10 +2,13 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import profile from "../../assets/images/profile.jpg";
 import Box from "@mui/material/Box";
+import { useMediaQuery } from "react-responsive";
 
 import "./About.scss";
 
 const About = () => {
+  const isPhone = useMediaQuery({ query: "(max-width: 767px)" });
+
   return (
     <Container
       maxWidth={false}
@@ -14,25 +17,38 @@ const About = () => {
       id="about"
     >
       <Container className="About-Content Variant">
-        <Typography variant="h2" align="center">
+        <Typography variant="h2" align="center" className="About-Title">
           Welcome!
         </Typography>
         <Container fixed className="About-Content">
           <img className="About-Img" src={profile} alt="profile-pic" />
-          <Typography className="About-Text" fontSize="18px">
-            I'm a passionate software engineer with a proven track record of
-            thriving in collaborative team environments. I'm also a lifelong
-            learner who continuously seeks out new technologies and
-            methodologies to stay ahead in the ever-evolving tech landscape.
-            Known for a strong work ethic, dependability, and a commitment to
-            delivering high-quality solutions. Whether tackling complex problems
-            or contributing to team success, I bring energy, curiosity, and a
-            steadfast dedication to every project.
-          </Typography>
+          <Box className="About-Text">
+            <Typography
+              className="About-Copy"
+              fontSize={isPhone ? "18px" : "22px"}
+              gutterBottom
+            >
+              I'm a passionate software engineer with a proven track record of
+              thriving in collaborative team environments. I'm also a lifelong
+              learner who continuously seeks out new technologies and
+              methodologies to stay ahead in the ever-evolving tech landscape.
+            </Typography>
+            <Typography
+              className="About-Copy"
+              fontSize={isPhone ? "18px" : "22px"}
+              gutterBottom
+            >
+              Known for a strong work ethic, dependability, and a commitment to
+              delivering high-quality solutions. Whether tackling complex
+              problems or contributing to team success, I bring energy,
+              curiosity, and a steadfast dedication to every project.
+            </Typography>
+          </Box>
         </Container>
         <Container fixed className="About-Content Variant">
           <Typography variant="h4">Stack</Typography>
           <Box component={"section"} className="About-Stack">
+            <i className="About-Icon devicon-nodejs-plain-wordmark colored"></i>
             <i className="About-Icon devicon-html5-plain colored"></i>
             <i className="About-Icon devicon-sass-plain colored"></i>
             <i className="About-Icon devicon-javascript-plain colored"></i>
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,8 +1,17 @@
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import "./Projects.scss";
 
 const Projects = () => {
+  const handleClick = () => {
+    window.open(
+      "https://github.com/dansilvaUT",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  };
   return (
     <Container
       maxWidth={false}
@@ -10,9 +19,20 @@ const Projects = () => {
       className="Projects-Container"
       id="code"
     >
-      <Typography variant="h2" align="center">
-        Comming Soon!
+      <Typography variant="h2" align="center" className="Projects-Title">
+        Projects
       </Typography>
+      <Box className="Projects-Content">
+        <div className="Projects-Card">
+          <Typography>
+            I use GitHub to keep track of my projects repositories.
+          </Typography>
+          <i className="Projects-Icon devicon-github-original-wordmark"></i>
+          <Button color="secondary" onClick={handleClick}>
+            View GitHub
+          </Button>
+        </div>
+      </Box>
     </Container>
   );
 };
